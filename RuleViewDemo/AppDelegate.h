b79a6341544069a7b33c39dfcdfb103f1a786b7e
Haohao Zhang <ioszhanghaohao@163.com>
2016-11-10 15:12:05 +0800

    Initial Commit

---
+++ RuleViewDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RuleViewDemo
+//
+//  Created by 张豪豪 on 16/11/10.
+//  Copyright © 2016年 zhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ RuleViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuleViewDemo
+//
+//  Created by 张豪豪 on 16/11/10.
+//  Copyright © 2016年 zhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
